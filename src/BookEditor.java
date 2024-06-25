125492a4f1e5e96b0cb960ab69957d32b3f6820a
GosuCode <gosucode1945@gmail.com>
2024-06-25 17:00:19 +0545

    building ui using swing

---
+++ src/BookEditor.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class BookEditor extends JFrame{
+    private JTextField bookNameField;
+    private JComboBox genreComboBox;
+    private JCheckBox isTakenCheckBox;
+    private JTextField authorNameField;
+    private JButton saveButton;
+    private JButton cancelButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
